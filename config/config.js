8d91e8df29a82d1a319a5fd6866d04bda72f0246
Vannisa Pascal-Nelson <vannisaepascalnelson@gmail.com>
2020-06-29 01:41:44 -0400

    connecting to db from the cloud

--- config/config.js
+++ config/config.js
@@ -1,9 +1,20 @@
 const Sequelize=require('sequelize');
 
 
-const sequelize = new Sequelize('ppl_database', 'ppl_user', 'harouna', {
-  host: 'localhost',
-  dialect:  'postgres' 
+// const sequelize = new Sequelize('ppl_database', 'ppl_user', 'harouna', {
+//   host: 'localhost',
+//   dialect:  'postgres' 
+// });
+
+
+const sequelize = new Sequelize('heroku_42902aead1b8139', 'b0c65c38ce144d', '96f34267', {
+  host: 'us-cdbr-east-02.cleardb.com',
+  dialect:  'mysql' 
 });
+
+
+
+
+
 
 module.exports=sequelize;
